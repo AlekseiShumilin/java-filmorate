8548c3cb5b5020634b0cadd4a560f249a5d60e9d
Aleksei <alekseyshumilin2013@yandex.ru>
2023-04-07 23:37:17 +0300

    feat: добавить интеграцию с базой данных

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/MpaController.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/mpa")
+public class MpaController {
+    public MpaDbStorage mpaStorage;
+
+    public MpaController(MpaDbStorage mpaStorage) {
+        this.mpaStorage = mpaStorage;
+    }
+
+    @GetMapping
+    public List<Mpa> getAllMpa() {
+        log.info("getAllMpa.");
+        return mpaStorage.getAllMpa();
+    }
+
+    @GetMapping("/{id}")
+    public Mpa getMpaById(@PathVariable int id) {
+        log.info("getMpaById.");
+        return mpaStorage.getMpaById(id);
+    }
+}
