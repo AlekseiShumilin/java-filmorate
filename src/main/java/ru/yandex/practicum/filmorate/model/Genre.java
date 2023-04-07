ddb9e23718338a45db2a30b6d57bf1d9ed8a73ea
Aleksei <alekseyshumilin2013@yandex.ru>
2023-04-07 22:50:07 +0300

    feat: добавить интеграцию с базой данных

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+import lombok.Data;
+
+@Data
+public class Genre {
+    String name;
+    int id;
+
+    public Genre(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+}
--- src/test/java/ru/yandex/practicum/filmorate/FilmValidationAndServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmValidationAndServiceTest.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.IncorrectParameterException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.services.FilmService;
 import ru.yandex.practicum.filmorate.services.FilmValidator;
@@ -14,10 +15,9 @@ import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class FilmValidationAndServiceTest {
     FilmStorage filmStorage = new InMemoryFilmStorage();
@@ -38,6 +38,15 @@ class FilmValidationAndServiceTest {
     User user1;
     User user2;
     User user3;
+    void initialize(){
+        film0 = new Film("Film0", "Film0 description",
+                LocalDate.of(2000, 10, 1),
+                120, new Mpa("G", 1));
+        film1 = new Film("Film1", "Film1 description",
+                LocalDate.of(2000, 10, 1), 120, new Mpa("G", 1));
+        film2 = new Film("Film2", "Film2 description",
+                LocalDate.of(2000, 10, 1), 120, new Mpa("G", 1));
+    }
 
     @Test
     void addNullFilm() {
@@ -46,10 +55,7 @@ class FilmValidationAndServiceTest {
 
     @Test
     void addNormalFilm() {
-        film0 = new Film("Film0", "Film0 description",
-                LocalDate.of(2000, 10, 1), 120);
-        film1 = new Film("Film1", "Film1 description",
-                LocalDate.of(2000, 10, 1), 120);
+        initialize();
         filmController.addFilm(film0);
         filmController.addFilm(film1);
         assertEquals(filmController.filmStorage.getFilm(1), film0);
@@ -58,8 +64,7 @@ class FilmValidationAndServiceTest {
     }
     @Test
     void deleteFilm(){
-        film1 = new Film("Film1", "Film1 description",
-                LocalDate.of(2000, 10, 1), 120);
+        initialize();
         filmController.addFilm(film1);
         filmStorage.deleteFilm(1);
         assertTrue(filmStorage.getAllFilms().isEmpty());
@@ -68,28 +73,29 @@ class FilmValidationAndServiceTest {
 
     @Test
     void addFilmEmptyName() {
-        film2 = new Film("", "Film2 description", LocalDate.of(2000, 10, 1), 120);
+        film2 = new Film("", "Film2 description",
+                LocalDate.of(2000, 10, 1), 120, new Mpa("G", 1));
         assertThrows(ValidationException.class, () -> filmController.addFilm(film2));
     }
 
     @Test
     void addFilmBlankName() {
         film3 = new Film("  ", "Film3 description",
-                LocalDate.of(2000, 10, 1), 120);
+                LocalDate.of(2000, 10, 1), 120, new Mpa("G", 1));
         assertThrows(ValidationException.class, () -> filmController.addFilm(film3));
     }
 
     @Test
     void addFilmWrongReleaseDate() {
         film4 = new Film("Film4", "Film4 description",
-                LocalDate.of(1895, 12, 27), 120);
+                LocalDate.of(1895, 12, 27), 120, new Mpa("G", 1));
         assertThrows(ValidationException.class, () -> filmController.addFilm(film4));
     }
 
     @Test
     void addFilmEarliestReleaseDate() {
         film5 = new Film("Film5", "Film5 description",
-                LocalDate.of(1895, 12, 28), 120);
+                LocalDate.of(1895, 12, 28), 120, new Mpa("G", 1));
         filmController.addFilm(film5);
         assertEquals(filmController.filmStorage.getFilm(1), film5);
     }
@@ -100,49 +106,46 @@ class FilmValidationAndServiceTest {
                 "Description Description Description Description Description Description Description" +
                         " Description Description Description Description Description Description Description" +
                         " Description Description Description",
-                LocalDate.of(2000, 10, 1), 120);
+                LocalDate.of(2000, 10, 1), 120, new Mpa("G", 1));
         assertThrows(ValidationException.class, () -> filmController.addFilm(film6));
     }
 
     @Test
     void addFilmWrongDuration() {
         film7 = new Film("Film7", "Film7 description",
-                LocalDate.of(2000, 10, 1), 0);
+                LocalDate.of(2000, 10, 1), 0, new Mpa("G", 1));
         assertThrows(ValidationException.class, () -> filmController.addFilm(film7));
     }
 
     @Test
     void addFilmNegativeDuration() {
         film8 = new Film("Film8", "Film8 description",
-                LocalDate.of(2000, 10, 1), -1);
+                LocalDate.of(2000, 10, 1), -1, new Mpa("G", 1));
         assertThrows(ValidationException.class, () -> filmController.addFilm(film8));
     }
 
     @Test
     void updateFilm() {
-        film0 = new Film("Film0", "Film0 description",
-                LocalDate.of(2000, 10, 1), 120);
-        film1 = new Film("Film1", "Film1 description",
-                LocalDate.of(1999, 10, 1), 100);
+        initialize();
         filmController.addFilm(film0);
         film1.setId(1);
         filmController.updateFilm(film1);
-        assertEquals(filmController.filmStorage.getFilm(1).getName(), "Film1");
-        assertEquals(filmController.filmStorage.getFilm(1).getDescription(), "Film1 description");
-        assertEquals(filmController.filmStorage.getFilm(1).getDuration(), 100);
-        assertEquals(filmController.filmStorage.getFilm(1).getReleaseDate(),
-                LocalDate.of(1999, 10, 1));
+        assertEquals(filmController.filmStorage.getFilm(1).get().getName(), "Film1");
+        assertEquals(filmController.filmStorage.getFilm(1).get().getDescription(), "Film1 description");
+        assertEquals(filmController.filmStorage.getFilm(1).get().getDuration(), 120);
+        assertEquals(filmController.filmStorage.getFilm(1).get().getReleaseDate(),
+                LocalDate.of(2000, 10, 1));
 
     }
 
     @Test
     void likeAndUnlikeFilm() {
-        film1 = new Film("Film1", "Film1 description",
-                LocalDate.of(2000, 10, 1), 120);
-        filmController.addFilm(film1);
+        initialize();
         user1 = new User("User1", "user1@mail.ru", "loginUser1",
                 LocalDate.of(1987, 1, 1));
         userStorage.addUser(user1);
+        filmStorage.addFilm(film1);
+
         filmController.likeFilm(1, 1);
         assertTrue(film1.getLikes().contains((long) 1));
         filmController.unlikeFilm(1, 1);
@@ -152,10 +155,7 @@ class FilmValidationAndServiceTest {
 
     @Test
     void getAllFilms() {
-        film0 = new Film("Film0", "Film0 description",
-                LocalDate.of(2000, 10, 1), 120);
-        film1 = new Film("Film1", "Film1 description",
-                LocalDate.of(2000, 10, 1), 120);
+        initialize();
         filmController.addFilm(film1);
         filmController.addFilm(film0);
         assertTrue(filmController.filmStorage.getAllFilms().contains(film0));
@@ -164,12 +164,7 @@ class FilmValidationAndServiceTest {
 
     @Test
     void getMostPopularFilm() {
-        film0 = new Film("Film0", "Film0 description",
-                LocalDate.of(2000, 10, 1), 120);
-        film1 = new Film("Film1", "Film1 description",
-                LocalDate.of(2000, 10, 1), 120);
-        film2 = new Film("Film2", "Film2 description",
-                LocalDate.of(2000, 10, 1), 120);
+        initialize();
         filmController.addFilm(film1);
         filmController.addFilm(film0);
         filmController.addFilm(film2);
@@ -197,8 +192,7 @@ class FilmValidationAndServiceTest {
 
     @Test
     void getFilmById() {
-        film0 = new Film("Film0", "Film0 description",
-                LocalDate.of(2000, 10, 1), 120);
+        initialize();
         filmController.addFilm(film0);
         assertEquals(film0, filmController.getFilmById(1));
         assertThrows(FilmNotFoundException.class, () -> filmController.getFilmById(10));
