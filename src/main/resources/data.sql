8f7355f66b67f7d4a2fde3167c8eeb2433d18371
Aleksei <alekseyshumilin2013@yandex.ru>
2023-04-07 23:19:26 +0300

    feat: добавить интеграцию с базой данных

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+insert into MPA (id, name)
+values (1, 'G'),
+(2, 'PG'),
+(3, 'PG-13'),
+(4, 'R'),
+(5, 'NC-17');
+
+INSERT INTO GENRES (ID, name)
+VALUES (1, 'Комедия'),
+(2, 'Драма'),
+(3, 'Мультфильм'),
+(4, 'Триллер'),
+(5, 'Документальный'),
+(6, 'Боевик');
+----
+--update MPA set name = 'G' where id = 1;
+--update MPA set name = 'PG', id = 2;
+--update MPA set name = 'PG-13', id = 3;
+--update MPA set name = 'R', id = 4;
+--update MPA set name = 'NC-17', id = 5;
+
